32ed141b9adfa90227a8284e97459a440a43a580
BDdatanj <39561894+BDdatanj@users.noreply.github.com>
2019-01-31 13:21:12 -0600

    Update Budget_Load_Yearly.sql

--- Budget_Load_Yearly.sql
+++ Budget_Load_Yearly.sql
@@ -13,6 +13,7 @@ Tables used
   
 Updates: v1 - 1/29/2018
 	v2 - 1/29/2019 - Added link to our accountint table.  Will look to auto load the data from the query
+	v3 - 1/31/2019 - The initial query is for testing to get the data.  The second 2 queries are for inserting the data.
 ***************************/
 Select
 	ad.[date],
@@ -110,3 +111,133 @@ Group by
 	Else di.Description
 	End
 	--DAY(EOMONTH(Convert(Datetime, convert(varchar(10), gl.TimePeriod))))
+
+/***********
+Insert the data from the above query straight into the budget table
+***********/
+
+insert into it.dbo.BUDGET
+select
+
+	ad.[date],
+	ad.Year_Number as Budget_Year,
+	--left(gl.TimePeriod, 4) as Year_Estimate,
+	ad.Month_Number as Budget_Month,
+	--substring(cast(gl.TimePeriod as varchar(8)), 5, 2)  as Month_Estimate,
+	--Convert(Datetime, convert(varchar(10), gl.TimePeriod)) as date,
+	--di.Description as Location_Description,
+	case
+		when di.Code = 160 then 'CONTAINERSTORE'
+		when di.Code = 147 then 'FREDMEYER'
+		when di.Code = 137 then 'CANADIANTIRE'
+		when di.Code = 179 then 'BJ''SWHOLESALE'
+		when di.Code = 165 then 'BEDBATHBEYOND'
+		when di.Code = 260 then 'WHOLESALE'
+		when di.Code = 224 then 'WEWORK'
+		when di.Code = 225 then 'STEELCASE'
+		when di.Code = 222 then 'TRADEPROJECT'
+		when di.Code = 271 then 'RETAIL SOHO'
+		when di.Code = 279 then 'RETAIL SEATTLE'
+		when di.Code = 273 then 'RETAIL SF'
+		when di.Code = 278 then 'RETAIL NOMAD'
+		when di.Code = 277 then 'RETAIL LA'
+		when di.Code = 276 then 'RETAIL CHICAGO'
+		when di.Code = 274 then 'RETAIL AUSTIN'
+		when di.Code = 270 then 'ABC'
+		when di.Code = 272 then 'RETAIL LA'
+		when di.Code = 221 then 'OFFICEFURN'
+		when di.Code = 220 then 'ID/ARCH'
+		when di.Code = 210 then 'Direct Web'
+		when di.Code = 212 then 'Direct Other'
+		when di.Code = 275 then 'RETAIL MPLS'
+		when di.code = 280 then 'Retail Portland'
+		when di.code = 281 then 'Retail New Store'
+	Else di.Description
+	End as [Customer Class - Original],
+	--DAY(EOMONTH(Convert(Datetime, convert(varchar(10), gl.TimePeriod)))) as days_in_month,
+	-sum(gl.Value1 / DAY(EOMONTH(Convert(Datetime, convert(varchar(10), gl.TimePeriod))))) as avg_day_budget_per_month
+	-- -sum(gl.value1) as total_Budget
+
+FROM
+	[dev-sql1].BI360DW.dbo.f_Trans_GL gl
+	inner join
+	[dev-sql1].BI360DW.dbo.d_account acct
+		on gl.account = acct.MemberId
+	Inner join
+	[dev-sql1].BI360DW.dbo.d_dim2 di
+		on gl.Dim2 = di.MemberID
+	Inner Join
+	[bludot-sql1].it.dbo.acct_date ad 
+		on left(gl.TimePeriod, 4) = ad.Year_Number
+		and substring(cast(gl.TimePeriod as varchar(8)), 5, 2) = ad.Month_Number
+where
+	-- acct.code in ('40000', '45000', '46000', '47000') -- Represent the Accounting codes we load budget into
+	acct.code between '40000' and '49999'
+	and acct.code not like '47%'
+	and gl.Scenario in (3) -- Represents the years of budget loading.  So when we add 2019 we would have to code something in
+	and left(gl.TimePeriod, 4) = 2019
+	and ad.Year_Number = 2019
+Group by
+	ad.[date],
+	ad.Year_Number,
+	--left(gl.TimePeriod, 4),
+	ad.Month_Number,
+	--substring(cast(gl.TimePeriod as varchar(8)), 5, 2),
+	--Convert(Datetime, convert(varchar(10), gl.TimePeriod)),
+	--di.Description,
+	case
+		when di.Code = 160 then 'CONTAINERSTORE'
+		when di.Code = 147 then 'FREDMEYER'
+		when di.Code = 137 then 'CANADIANTIRE'
+		when di.Code = 179 then 'BJ''SWHOLESALE'
+		when di.Code = 165 then 'BEDBATHBEYOND'
+		when di.Code = 260 then 'WHOLESALE'
+		when di.Code = 224 then 'WEWORK'
+		when di.Code = 225 then 'STEELCASE'
+		when di.Code = 222 then 'TRADEPROJECT'
+		when di.Code = 271 then 'RETAIL SOHO'
+		when di.Code = 279 then 'RETAIL SEATTLE'
+		when di.Code = 273 then 'RETAIL SF'
+		when di.Code = 278 then 'RETAIL NOMAD'
+		when di.Code = 277 then 'RETAIL LA'
+		when di.Code = 276 then 'RETAIL CHICAGO'
+		when di.Code = 274 then 'RETAIL AUSTIN'
+		when di.Code = 270 then 'ABC'
+		when di.Code = 272 then 'RETAIL LA'
+		when di.Code = 221 then 'OFFICEFURN'
+		when di.Code = 220 then 'ID/ARCH'
+		when di.Code = 210 then 'Direct Web'
+		when di.Code = 212 then 'Direct Other'
+		when di.Code = 275 then 'RETAIL MPLS'
+		when di.code = 280 then 'Retail Portland'
+		when di.code = 281 then 'Retail New Store'
+	Else di.Description
+	End
+	--DAY(EOMONTH(Convert(Datetime, convert(varchar(10), gl.TimePeriod))))
+
+/*****************
+taking records from 2018 that have no 2019 budget and putting them into the system for 2019 with a 0
+*********/
+				   
+insert into it.dbo.BUDGET	
+Select
+	ad.[date],
+	ad.Year_Number,
+	ad.Month_Number,
+	c.[Customer Class - Original],
+	0 as [Budget]
+from
+	it.dbo.acct_date ad
+	Inner join
+(
+Select b.[Customer Class - Original]
+from
+
+(Select distinct [Customer Class - Original] from it.dbo.BUDGET where Budget_Year = 2019) a
+right join 
+(Select distinct [Customer Class - Original] from it.dbo.BUDGET where Budget_Year = 2018) b
+	on a.[Customer Class - Original] = b.[Customer Class - Original]
+Where
+	a.[Customer Class - Original] is null
+) c
+	on ad.Year_Number = 2019
